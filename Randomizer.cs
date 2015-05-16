ae932e9463039c0edce499f51fc2f71ec1e2afc2
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-16 16:22:10 +0200

    Update docs

--- Randomizer.cs
+++ Randomizer.cs
@@ -6,6 +6,7 @@ namespace Randomizer {
     public class Randomizer : MonoBehaviour {
 
         #region FIELDS
+
         /// Helper variable.
         ///
         /// In-game time to next trigger.
@@ -15,23 +16,19 @@ namespace Randomizer {
         #endregion
 
         #region INSPECTOR FIELDS
-        /// Initial delay.
+
         [SerializeField]
         private float initDelay;
 
-        /// Time between consequent triggers.
         [SerializeField]
         private float interval;
 
-        /// Interval type.
         [SerializeField]
         private IntervalTypes intervalType;
 
-        /// Minimum interval.
         [SerializeField]
         private float minInterval;
 
-        /// Maximum interval.
         [SerializeField]
         private float maxInterval;
         #endregion
@@ -41,33 +38,44 @@ namespace Randomizer {
         /// <summary>
         /// The class output value. Use it to trigger actions in your game.
         /// </summary>
+        // todo rename to State
         public bool Result { get; private set; }
 
+        /// <summary>
         /// Initial delay.
+        /// </summary>
         public float InitDelay {
             get { return initDelay; }
             set { initDelay = value; }
         }
 
+        /// <summary>
         /// Time between consequent triggers.
+        /// </summary>
         public float Interval {
             get { return interval; }
             set { interval = value; }
         }
 
+        /// <summary>
         /// Interval type.
+        /// </summary>
         public IntervalTypes IntervalType {
             get { return intervalType; }
             set { intervalType = value; }
         }
 
+        /// <summary>
         /// Minimum interval.
+        /// </summary>
         public float MinInterval {
             get { return minInterval; }
             set { minInterval = value; }
         }
 
+        /// <summary>
         /// Maximum interval.
+        /// </summary>
         public float MaxInterval {
             get { return maxInterval; }
             set { maxInterval = value; }
@@ -85,7 +93,7 @@ namespace Randomizer {
         }
 
         private void Update () {
-            /// Handle 'Random' interval option.
+            // Handle 'Random' interval option.
             if (IntervalType == IntervalTypes.Random) {
                 // Wait random interval before trigger.
                 if (Time.time > timeToTrigger) {
@@ -107,6 +115,7 @@ namespace Randomizer {
         #region METHODS
 
         /// Method that triggers the '_result' in time intervals.
+        // todo rename
         private void Trigger() {
             // Change component state right after initial delay.
             Result = !Result;
@@ -121,6 +130,7 @@ namespace Randomizer {
             var triggerResultCoroutine = new Task(TriggerResult());
         }
 
+        // todo rename to ToggleState.
         private IEnumerator TriggerResult() {
             while (true) {
                 Result = !Result;
