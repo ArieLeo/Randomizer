7b3804193caf2943c45c2e5d5736a3ba567fc8e2
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-16 13:44:25 +0200

    Initial commit

---
+++ Editor/RandomizerEditor.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using OneDayGame;
+
+[CustomEditor(typeof(Randomizer))]
+public class RandomizerEditor: GameComponentEditor {
+
+	private SerializedProperty _initDelay;
+	private SerializedProperty _interval;
+	private SerializedProperty _intervalType;
+	private SerializedProperty _minInterval;
+	private SerializedProperty _maxInterval;
+
+	public override void OnEnable() {
+		base.OnEnable();
+
+		_initDelay = serializedObject.FindProperty("_initDelay");
+		_interval = serializedObject.FindProperty("_interval");
+		_intervalType = serializedObject.FindProperty("_intervalType");
+		_minInterval = serializedObject.FindProperty("_minInterval");
+		_maxInterval = serializedObject.FindProperty("_maxInterval");
+	}
+
+	public override void OnInspectorGUI() {
+		base.OnInspectorGUI();
+		//Randomizer script = (Randomizer)target;
+		serializedObject.Update();
+
+		EditorGUILayout.BeginHorizontal();
+		EditorGUIUtility.labelWidth = 70;
+		EditorGUILayout.PropertyField(
+				_initDelay,
+				GUILayout.MaxWidth(120));
+		EditorGUIUtility.labelWidth = 40;
+		EditorGUILayout.PropertyField(
+				_intervalType,
+				new GUIContent("Type", "Type of the interval applied."));
+		EditorGUILayout.EndHorizontal();
+
+		switch (_intervalType.enumValueIndex) {
+			case (int)Randomizer.IntervalTypes.Fixed:
+				EditorGUIUtility.labelWidth = 0;
+				EditorGUILayout.PropertyField(_interval);
+			break;
+			case (int)Randomizer.IntervalTypes.Random:
+				EditorGUILayout.BeginHorizontal();
+				EditorGUIUtility.labelWidth = 80;
+				EditorGUILayout.PropertyField(_minInterval);
+				EditorGUILayout.PropertyField(_maxInterval);
+				EditorGUILayout.EndHorizontal();
+			break;
+		}
+
+		serializedObject.ApplyModifiedProperties();
+		// Save changes
+		/*if (GUI.changed) {
+			EditorUtility.SetDirty(script);
+		}*/
+	}
+}
---
+++ Randomizer.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace OneDayGame {
+
+	public class Randomizer : GameComponent {
+
+		public enum IntervalTypes { Fixed, Random }
+
+		/// Result that the component returns.
+		///
+		/// This property can be read by another component in order to execute
+		/// action at a random intervals.
+		private bool _result;
+		public bool Result {
+			get { return _result; }
+		}
+
+		/// Initial delay.
+		[SerializeField]
+		private float _initDelay;
+
+		/// Time between consequent triggers.
+		[SerializeField]
+		private float _interval;
+
+		/// Interval type.
+		[SerializeField]
+		private IntervalTypes _intervalType;
+
+		/// Minimum interval.
+		[SerializeField]
+		private float _minInterval;
+
+		/// Maximum interval.
+		[SerializeField]
+		private float _maxInterval;
+
+		/// Helper variable.
+		///
+		/// In-game time to next trigger.
+		/// Used in 'Random' option.
+		private float _timeToTrigger;
+
+		public override void Awake() {
+			base.Awake();
+		}
+
+		public override void Start () {
+			base.Start();
+
+			// Handle 'Fixed' interval type.
+			if (_intervalType == IntervalTypes.Fixed) {
+				Invoke("Trigger", _initDelay);
+			}
+		}
+
+		public override void Update () {
+			base.Update();
+
+			/// Handle 'Random' interval option.
+			if (_intervalType == IntervalTypes.Random) {
+				// Wait random interval before trigger.
+				if (Time.time > _timeToTrigger) {
+					float interval;
+
+					// Calculate new random interval.
+					interval = Random.Range(_minInterval, _maxInterval);
+					// Update time to next trigger.
+					_timeToTrigger = Time.time + interval;
+
+					// Trigger.
+					_result = !_result;
+				}
+			}
+		}
+
+		public override void FixedUpdate() {
+			base.FixedUpdate();
+		}
+
+		public override void LateUpdate() {
+			base.LateUpdate();
+		}
+
+		/// Method that triggers the '_result' in time intervals.
+		private void Trigger() {
+			// Change component state right after initial delay.
+			_result = !_result;
+
+			//  Change controller state in fixed intervals.
+			StartCoroutine(Timer.Start(
+						_interval,
+						true,
+						() => { _result = !_result; }
+						));
+		}
+	}
+}
