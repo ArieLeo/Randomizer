89803c0196f0bafc9f50bf89ecd79fd32ee453fb
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-19 09:16:52 +0200

    Add licence info

--- Enums/IntervalTypes.cs
+++ Enums/IntervalTypes.cs
@@ -1,4 +1,9 @@
-﻿namespace Randomizer {
+﻿// Copyright (c) 2015 Bartłomiej Wołk (bartlomiejwolk@gmail.com)
+//  
+// This file is part of the Randomizer extension for Unity.
+// Licensed under the MIT license. See LICENSE file in the project root folder.
+
+namespace Randomizer {
 
     public enum IntervalTypes { Fixed, Random }
 
